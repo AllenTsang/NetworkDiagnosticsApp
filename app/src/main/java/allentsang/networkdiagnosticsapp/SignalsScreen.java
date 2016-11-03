1b9c67009cede10311b6d36b638fadbc14264618
Allen Tsang <allen_tsang@hotmail.com>
2016-11-03 11:35:31 -0700

    Added nearby signals activity

---
+++ app/src/main/java/allentsang/networkdiagnosticsapp/SignalsScreen.java
@@ -0,0 +1,13 @@
+package allentsang.networkdiagnosticsapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignalsScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signals_screen);
+    }
+}
