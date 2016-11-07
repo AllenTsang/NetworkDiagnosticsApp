889951ba9e11cc515aa17b5c34817304dbb31a59
Allen Tsang <allen_tsang@hotmail.com>
2016-11-07 01:54:37 -0800

    Getting basic info from network

--- app/src/main/java/allentsang/networkdiagnosticsapp/StartScreen.java
+++ app/src/main/java/allentsang/networkdiagnosticsapp/StartScreen.java
@@ -1,13 +1,28 @@
 package allentsang.networkdiagnosticsapp;
 
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class StartScreen extends AppCompatActivity {
+
+    WifiManager wifiManager;
+    WifiInfo wifiInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_screen);
+
+        wifiManager = (WifiManager)getSystemService(this.WIFI_SERVICE);
+        wifiInfo = wifiManager.getConnectionInfo();
+
+        ((TextView)findViewById(R.id.start_ssid_content)).setText(wifiInfo.getSSID());
+
+        int ipAddr = wifiInfo.getIpAddress();
+        String ipString = String.format("%d.%d.%d.%d", (ipAddr & 0xff), (ipAddr >> 8 & 0xff), (ipAddr >> 16 & 0xff), (ipAddr >> 24 & 0xff));
+        ((TextView)findViewById(R.id.start_ip_content)).setText(ipString);
     }
 }
