355fa5d72c89ef4d4df292feea67dc562da06814
Allen Tsang <allen_tsang@hotmail.com>
2016-11-08 01:22:48 -0800

    Added nearby signal switch

--- app/src/main/java/allentsang/networkdiagnosticsapp/StartScreen.java
+++ app/src/main/java/allentsang/networkdiagnosticsapp/StartScreen.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class StartScreen extends AppCompatActivity {
@@ -35,5 +36,16 @@ public class StartScreen extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.action_wifi:
+                
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
