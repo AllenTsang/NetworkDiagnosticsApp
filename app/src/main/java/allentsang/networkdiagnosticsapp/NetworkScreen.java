0640f58166f28bb82a4b0fdafcfc1667543271a5
Allen Tsang <allen_tsang@hotmail.com>
2016-11-10 04:19:15 -0800

    WifiP2P discovery

--- app/src/main/java/allentsang/networkdiagnosticsapp/NetworkScreen.java
+++ app/src/main/java/allentsang/networkdiagnosticsapp/NetworkScreen.java
@@ -1,9 +1,12 @@
 package allentsang.networkdiagnosticsapp;
 
+import android.content.Context;
 import android.content.Intent;
-import android.net.wifi.ScanResult;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pDeviceList;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -13,15 +16,15 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
 
 public class NetworkScreen extends AppCompatActivity {
 
     WifiManager wifiManager;
     WifiInfo wifiInfo;
+    Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,16 +67,24 @@ public class NetworkScreen extends AppCompatActivity {
         ((TextView)findViewById(R.id.ip_content)).setText(ipString);
 
         //Populate device list
-        ArrayList<String> list = new ArrayList<String>();
-        wifiManager.startScan();
-        List<ScanResult> results = wifiManager.getScanResults();
+        final ArrayList<String> list = new ArrayList<String>();
 
-        for(ScanResult sr : results) {
-            String s = String.format("SSID:\t%s\nStrength:\t%d dBm", sr.SSID, sr.level);
-            list.add(s);
-        }
+        WifiP2pManager P2pManager = (WifiP2pManager) getSystemService(WIFI_P2P_SERVICE);
+        WifiP2pManager.Channel c = P2pManager.initialize(this, getMainLooper(), null);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list);
-        ((ListView)findViewById(R.id.device_list)).setAdapter(adapter);
+        P2pManager.discoverPeers(c, null);
+
+        P2pManager.requestPeers(c, new WifiP2pManager.PeerListListener() {
+            @Override
+            public void onPeersAvailable(WifiP2pDeviceList peers) {
+                Collection<WifiP2pDevice> col = peers.getDeviceList();
+                for(WifiP2pDevice d : col) {
+                    String s = String.format("Name:\t%s\nAddress:\t%s", d.deviceName, d.deviceAddress);
+                    list.add(s);
+                }
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(context, android.R.layout.simple_list_item_1, list);
+                ((ListView)findViewById(R.id.device_list)).setAdapter(adapter);
+            }
+        });
     }
 }
