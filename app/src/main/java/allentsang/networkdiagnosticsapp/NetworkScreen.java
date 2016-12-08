66f1ae62934035db6528c37d2184b6de65b5ec10
Allen Tsang <allen_tsang@hotmail.com>
2016-12-07 19:24:51 -0800

    Cling library attempt, non-functional

--- app/src/main/java/allentsang/networkdiagnosticsapp/NetworkScreen.java
+++ app/src/main/java/allentsang/networkdiagnosticsapp/NetworkScreen.java
@@ -1,12 +1,15 @@
 package allentsang.networkdiagnosticsapp;
 
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.net.wifi.p2p.WifiP2pDevice;
 import android.net.wifi.p2p.WifiP2pDeviceList;
 import android.net.wifi.p2p.WifiP2pManager;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -16,6 +19,16 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.fourthline.cling.android.AndroidUpnpService;
+import org.fourthline.cling.android.AndroidUpnpServiceImpl;
+import org.fourthline.cling.android.FixedAndroidLogHandler;
+import org.fourthline.cling.model.meta.Device;
+import org.fourthline.cling.model.meta.LocalDevice;
+import org.fourthline.cling.model.meta.RemoteDevice;
+import org.fourthline.cling.registry.DefaultRegistryListener;
+import org.fourthline.cling.registry.Registry;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -26,6 +39,38 @@ public class NetworkScreen extends AppCompatActivity {
     WifiInfo wifiInfo;
     Context context = this;
 
+    private ArrayAdapter<DeviceDisplay> listAdapter;
+    //final ArrayList<String> list = new ArrayList<String>();
+
+    private BrowseRegistryListener registryListener = new BrowseRegistryListener();
+
+    private AndroidUpnpService upnpService;
+
+    private ServiceConnection serviceConnection = new ServiceConnection() {
+
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            upnpService = (AndroidUpnpService) service;
+
+            // Clear the list
+            listAdapter.clear();
+
+            // Get ready for future device advertisements
+            upnpService.getRegistry().addListener(registryListener);
+
+            // Now add all devices to the list we already know about
+            for (Device device : upnpService.getRegistry().getDevices()) {
+                registryListener.deviceAdded(device);
+            }
+
+            // Search asynchronously for all devices, they will respond soon
+            upnpService.getControlPoint().search();
+        }
+
+        public void onServiceDisconnected(ComponentName className) {
+            upnpService = null;
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +80,22 @@ public class NetworkScreen extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         refreshInfo(null);
+
+        // Fix the logging integration between java.util.logging and Android internal logging
+        org.seamless.util.logging.LoggingUtil.resetRootHandler(
+                new FixedAndroidLogHandler()
+        );
+
+        listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+        ((ListView)findViewById(R.id.device_list)).setAdapter(listAdapter);
+        //setListAdapter(listAdapter);
+
+        // This will start the UPnP service if it wasn't already started
+        getApplicationContext().bindService(
+                new Intent(this, AndroidUpnpServiceImpl.class),
+                serviceConnection,
+                Context.BIND_AUTO_CREATE
+        );
     }
 
     @Override
@@ -56,6 +117,16 @@ public class NetworkScreen extends AppCompatActivity {
         }
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (upnpService != null) {
+            upnpService.getRegistry().removeListener(registryListener);
+        }
+        // This will stop the UPnP service if nobody else is bound to it
+        getApplicationContext().unbindService(serviceConnection);
+    }
+
     public void refreshInfo(View view) {
         wifiManager = (WifiManager)getSystemService(this.WIFI_SERVICE);
         wifiInfo = wifiManager.getConnectionInfo();
@@ -73,6 +144,7 @@ public class NetworkScreen extends AppCompatActivity {
             ipContent.setText(ipString);
         }
 
+        /*
         //Populate device list
         final ArrayList<String> list = new ArrayList<String>();
 
@@ -96,6 +168,112 @@ public class NetworkScreen extends AppCompatActivity {
                 ArrayAdapter<String> adapter = new ArrayAdapter<String>(context, android.R.layout.simple_list_item_1, list);
                 ((ListView)findViewById(R.id.device_list)).setAdapter(adapter);
             }
-        });
+        });*/
+    }
+
+    protected class BrowseRegistryListener extends DefaultRegistryListener {
+
+        /* Discovery performance optimization for very slow Android devices! */
+        @Override
+        public void remoteDeviceDiscoveryStarted(Registry registry, RemoteDevice device) {
+            deviceAdded(device);
+        }
+
+        @Override
+        public void remoteDeviceDiscoveryFailed(Registry registry, final RemoteDevice device, final Exception ex) {
+            runOnUiThread(new Runnable() {
+                public void run() {
+                    Toast.makeText(
+                            NetworkScreen.this,
+                            "Discovery failed of '" + device.getDisplayString() + "': "
+                                    + (ex != null ? ex.toString() : "Couldn't retrieve device/service descriptors"),
+                            Toast.LENGTH_LONG
+                    ).show();
+                }
+            });
+            deviceRemoved(device);
+        }
+        /* End of optimization, you can remove the whole block if your Android handset is fast (>= 600 Mhz) */
+
+        @Override
+        public void remoteDeviceAdded(Registry registry, RemoteDevice device) {
+            deviceAdded(device);
+        }
+
+        @Override
+        public void remoteDeviceRemoved(Registry registry, RemoteDevice device) {
+            deviceRemoved(device);
+        }
+
+        @Override
+        public void localDeviceAdded(Registry registry, LocalDevice device) {
+            deviceAdded(device);
+        }
+
+        @Override
+        public void localDeviceRemoved(Registry registry, LocalDevice device) {
+            deviceRemoved(device);
+        }
+
+        public void deviceAdded(final Device device) {
+            System.out.println("adding device...");
+            runOnUiThread(new Runnable() {
+                public void run() {
+                    DeviceDisplay d = new DeviceDisplay(device);
+                    int position = listAdapter.getPosition(d);
+                    if (position >= 0) {
+                        // Device already in the list, re-set new value at same position
+                        listAdapter.remove(d);
+                        listAdapter.insert(d, position);
+                    } else {
+                        listAdapter.add(d);
+                    }
+                }
+            });
+        }
+
+        public void deviceRemoved(final Device device) {
+            runOnUiThread(new Runnable() {
+                public void run() {
+                    listAdapter.remove(new DeviceDisplay(device));
+                }
+            });
+        }
+    }
+
+    protected class DeviceDisplay {
+
+        Device device;
+
+        public DeviceDisplay(Device device) {
+            this.device = device;
+        }
+
+        public Device getDevice() {
+            return device;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            DeviceDisplay that = (DeviceDisplay) o;
+            return device.equals(that.device);
+        }
+
+        @Override
+        public int hashCode() {
+            return device.hashCode();
+        }
+
+        @Override
+        public String toString() {
+            String name =
+                    getDevice().getDetails() != null && getDevice().getDetails().getFriendlyName() != null
+                            ? getDevice().getDetails().getFriendlyName()
+                            : getDevice().getDisplayString();
+            // Display a little star while the device is being loaded (see performance optimization earlier)
+            return device.isFullyHydrated() ? name : name + " *";
+        }
     }
 }
